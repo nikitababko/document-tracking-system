788ed74f2d30dbac958d3c8f1983df8fcfb56170
Nikita Babko <niki.babko@gmail.com>
2021-06-09 22:16:20 +0700

    Deleted comments.

--- server/src/controllers/DocumentController.js
+++ server/src/controllers/DocumentController.js
@@ -2,21 +2,7 @@ const DocumentModel = require('../models/DocumentModel');
 
 const DocumentController = {
   uploadDocument: async (req, res) => {
-    // const newpath = __dirname + '/files/';
-    // const file = req.files.file;
-    // const filename = file.name;
-
-    // console.log(req.files);
-
-    // file.mv(`${newpath}${filename}`, (err) => {
-    //   if (err) {
-    //     res.status(500).send({ message: 'File upload failed', code: 200 });
-    //   }
-    //   res.status(200).send({ message: 'File Uploaded', code: 200 });
-    // });
     try {
-      // const { name } = user;
-
       const newDocument = new DocumentModel({
         name: 'test',
       });
