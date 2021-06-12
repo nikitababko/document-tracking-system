e47d8778afa2f96b5235fc4d8bc0c778258f7a2a
Nikita Babko <niki.babko@gmail.com>
2021-06-12 13:32:41 +0700

    Adde link to group.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import VKGroup from './VKGroup';
 
 import './index.scss';
+import Feedback from './Feedback';
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <div className="container">
         <div className="footer__item">
           <VKGroup />
@@ -24,20 +25,22 @@ const Footer = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5438.9596213265!2d82.9149749823504!3d55.029182916980616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdf828793aa57afaa!2z0KHQuNCx0LjRgNGB0LrQuNC5INCz0L7RgdGD0LTQsNGA0YHRgtCy0LXQvdC90YvQuSDRg9C90LjQstC10YDRgdC40YLQtdGCINCy0L7QtNC90L7Qs9C-INGC0YDQsNC90YHQv9C-0YDRgtCw!5e0!3m2!1sru!2sru!4v1484030978245"
             width="100%"
             height="300px"
-            frameborder="0"
+            frameBorder="0"
             style={{ border: 0 }}
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
 
         <div className="footer__item">
+          <Feedback />
+
           <div>
             <h3 className="footer__item-title">
               ФГБОУ ВО
               <em>
                 <p>
-                  "Сибирский государственный университет водного
-                  транспорта"
+                  "Сибирский государственный <br /> университет водного
+                  <br /> транспорта"
                 </p>
               </em>
             </h3>
@@ -95,7 +98,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
