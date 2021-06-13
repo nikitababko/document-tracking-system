2364021c4de714c67b39a3d167c6ed5ffa07801f
Nikita Babko <niki.babko@gmail.com>
2021-06-13 11:29:27 +0700

    Replaced Router with Tabs.

--- client/src/components/Tasks/TaskPage/index.js
+++ client/src/components/Tasks/TaskPage/index.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Menu, Switch, Divider } from 'antd';
-import {
-  MailOutlined,
-  CheckCircleOutlined,
-  QuestionCircleOutlined,
-  EditOutlined,
-} from '@ant-design/icons';
+import { Tabs } from 'antd';
 
 import './index.scss';
 import { NotFound, TaskCreate } from 'components';
@@ -19,89 +13,29 @@ const TaskPage = () => {
    * ! Временное решение
    * */
   const [filesArray, setFilesArray] = useState([]);
-  const [filesArrayTimeout, setFilesArrayTimeout] = useState([]);
 
-  useEffect(() => {
-    setFilesArrayTimeout(filesArray.length - filesArray.length + 1);
-
-    setTimeout(() => {
-      setFilesArrayTimeout([]);
-    }, 3000);
-  }, [filesArray]);
-
-  const auth = useSelector((state) => state.auth);
-  const { isLogged } = auth;
+  // Tabs
+  const { TabPane } = Tabs;
 
   return (
-    <div className="tasks" style={{ display: 'flex' }}>
-      <Router>
-        <div className="tasks__left">
-          <Menu
-            style={{ width: 256 }}
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            mode="inline"
-          >
-            <Menu.Item disabled>
-              <h2>Мои задачи</h2>
-            </Menu.Item>
-
-            <Menu.Item key="1" icon={<MailOutlined />}>
-              <Link to="/tasks/new">
-                <span className="text">Новые</span>
-                <div className="count-wrapper">
-                  <span className="count">
-                    {filesArrayTimeout === 1 ? 1 : 0}
-                  </span>
-                </div>
-              </Link>
-            </Menu.Item>
-
-            <Menu.Item key="2" icon={<QuestionCircleOutlined />}>
-              <Link to="/tasks/progress">
-                <span className="text">Выполняются</span>
-                <div className="count-wrapper">
-                  <span className="count">
-                    {filesArray.length ? filesArray.length : 0}
-                  </span>
-                </div>
-              </Link>
-            </Menu.Item>
-
-            <Menu.Item key="3" icon={<CheckCircleOutlined />}>
-              <Link to="/tasks/finished">
-                <span className="text">Завершенные</span>
-                <div className="count-wrapper">
-                  <span className="count">0</span>
-                </div>
-              </Link>
-            </Menu.Item>
-
-            <Menu.Item key="4" icon={<EditOutlined />}>
-              <Link to="/tasks/create">
-                <span className="text">Создать</span>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        </div>
-
-        <div className="tasks__rigth">
-          <Route exact path="/tasks/create">
-            {isLogged ? (
-              <TaskCreate
-                filesArray={filesArray}
-                setFilesArray={setFilesArray}
-              />
-            ) : (
-              NotFound
-            )}
-          </Route>
-
-          <Route exact path="/tasks/new">
-            {isLogged ? <TaskNew /> : NotFound}
-          </Route>
-        </div>
-      </Router>
+    <div className="tasks">
+      <Tabs defaultActiveKey="1" centered type="line" tabPosition="left">
+        <TabPane tab="Новые" key="1">
+          <TaskNew />
+        </TabPane>
+        <TabPane tab="Выполняются" key="2">
+          asdasd
+        </TabPane>
+        <TabPane tab="Завершенные" key="3">
+          asdasd
+        </TabPane>
+        <TabPane tab="Создать" key="4">
+          <TaskCreate
+            filesArray={filesArray}
+            setFilesArray={setFilesArray}
+          />
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
