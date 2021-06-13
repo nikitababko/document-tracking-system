4409687001593a1b077de5493c148d45a69c3c14
Nikita Babko <niki.babko@gmail.com>
2021-06-13 11:24:25 +0700

    Deleted text.

--- client/src/components/Tasks/TaskNew/index.js
+++ client/src/components/Tasks/TaskNew/index.js
@@ -27,11 +27,6 @@ const TaskNew = () => {
   };
   // Antd
   const { Panel } = Collapse;
-  const text = `
-    A dog is a type of domesticated animal.
-    Known for its loyalty and faithfulness,
-    it can be found as a welcome guest in many households across the world.
-  `;
 
   // Steps
   const { Step } = Steps;
