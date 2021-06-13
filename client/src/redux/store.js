5b248d30f4ae776c7226b1fce1c5ef9de4b99793
Nikita Babko <niki.babko@gmail.com>
2021-06-13 11:21:02 +0700

    Added redux-thunk.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/';
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 const DataProvider = ({ children }) => {
