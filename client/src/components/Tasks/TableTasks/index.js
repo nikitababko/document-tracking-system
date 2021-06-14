58abcae43e4d94da9416e5f389cd9285c40e2878
Nikita Babko <niki.babko@gmail.com>
2021-06-14 20:37:15 +0700

    Removed empty imports.

--- client/src/components/Tasks/TableTasks/index.js
+++ client/src/components/Tasks/TableTasks/index.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-
-import { fetchAllDocuments } from 'redux/actions/documentAction';
 
 import './index.scss';
 
