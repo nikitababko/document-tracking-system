3c6d7e2fdca7f4de445c46758c24836752238fe5
Nikita Babko <niki.babko@gmail.com>
2021-06-17 01:59:21 +0700

    Fix mail options.

--- server/src/controllers/SendMail.js
+++ server/src/controllers/SendMail.js
@@ -43,7 +43,7 @@ const sendEmail = (to, url, txt) => {
     html: `
       <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
       <h2 style="text-align: center; text-transform: uppercase;color: teal;">Добро пожаловать!</h2>
-      <p>Поздравляю! Вы почти готовы начать пользоваться электронной библиотекой СГУВТ. Просто нажмите кнопку ниже, чтобы подтвердить свой адрес электронной почты.
+      <p>Поздравляю! Вы почти готовы начать пользоваться электронным документооборотом СГУВТа. Просто нажмите кнопку ниже, чтобы подтвердить свой адрес электронной почты.
       </p>
       
       <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
